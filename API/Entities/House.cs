9f78bc005f488c139bf95d130618a55e006a44f2
Octavian Milea <octavian_milea@outlook.com>
2020-11-16 06:42:47 -0800

    skeleton

---
+++ API/Entities/House.cs
@@ -0,0 +1,27 @@
+﻿namespace dbtest
+{
+    public class House
+    {
+        public double Id { get; set; }
+        public string Date { get; set; }
+        public double Price { get; set; }
+        public int Bedrooms { get; set; }
+        public float Bathrooms { get; set; }
+        public int Sqft_living { get; set; }
+        public int Sqft_lot { get; set; }
+        public float Floors { get; set; }
+        public int Waterfront { get; set; }
+        public int View { get; set; }
+        public int Condition { get; set; }
+        public int Grade { get; set; }
+        public int Sqft_above { get; set; }
+        public int Sqft_basement { get; set; }
+        public int Yr_built { get; set; }
+        public int Yr_renovated { get; set; }
+        public int Zipcode { get; set; }
+        public float Lat { get; set; }
+        public float Longg { get; set; }
+        public int Sqft_living15 { get; set; }
+        public int Sqft_lot15 { get; set; }
+    }
+}
---
+++ API/Entities/HouseDatabase.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
+
+
+namespace dbtest
+{
+    public class HouseDatabase : DbContext
+    {
+        public DbSet<House> Houses { get; set; }
+
+        private static bool _created = false;
+        public HouseDatabase()
+        {
+            if (!_created)
+            {
+                _created = true;
+                Database.EnsureDeleted();
+                Database.EnsureCreated();
+            }
+            if(Houses.ToList().Count() == 0)
+            {
+                loadFromCSV("kc_house_data.csv");
+            }
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionbuilder)
+        {
+            optionbuilder.UseSqlite(@"Data Source=Baza.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<House>().ToTable("Houses");
+        }
+
+        public void loadFromCSV(string filename)
+        {
+            var readcsv = File.ReadAllText(filename);
+            string[] rows = readcsv.Split('\n');
+            var houses = this.Set<House>();
+
+            for (int i = 1; i <= 50; i++)
+            {
+                string[] cells = rows[i].Split(',');
+                House house = this.createHouse(cells);
+                houses.Add(house);
+                this.SaveChanges();
+            }
+        }
+
+        private House createHouse(string[] cells)
+        {
+            House house = new House
+            {
+                Id = Convert.ToDouble(cells[0].Trim('"')),
+                Date = cells[1],
+                Price = Convert.ToDouble(cells[2]),
+                Bedrooms = Convert.ToInt32(cells[3]),
+                Bathrooms = float.Parse(cells[4]),
+                Sqft_living = Convert.ToInt32(cells[5]),
+                Sqft_lot = Convert.ToInt32(cells[6]),
+                Floors = float.Parse(cells[7].Trim('"')),
+                Waterfront = Convert.ToInt32(cells[8]),
+                View = Convert.ToInt32(cells[9]),
+                Condition = Convert.ToInt32(cells[10]),
+                Grade = Convert.ToInt32(cells[11]),
+                Sqft_above = Convert.ToInt32(cells[12]),
+                Sqft_basement = Convert.ToInt32(cells[13]),
+                Yr_built = Convert.ToInt32(cells[14]),
+                Yr_renovated = Convert.ToInt32(cells[15]),
+                Zipcode = Convert.ToInt32(cells[16].Trim('"')),
+                Lat = float.Parse(cells[17]),
+                Longg = float.Parse(cells[18]),
+                Sqft_living15 = Convert.ToInt32(cells[19]),
+                Sqft_lot15 = Convert.ToInt32(cells[20])
+            };
+
+            return house;
+        }
+                
+    }
+}
